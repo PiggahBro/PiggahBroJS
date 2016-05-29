6fb8eb009367fba2146be91bc923ad793eac311f
Piggah Bro <piggahbro@gmail.com>
2016-05-28 21:52:16 -0400

    Update and rename PiggahBro.js to piggahbro.js

--- piggahbro.js
+++ piggahbro.js
@@ -15,45 +15,171 @@ var Saves = new DS();
 var CTXs = new DS();
 
 var PB = {
-  file: {
-    create: function(content, opts) {
-      // Generate data
-      var d = new Date()
-      var newBlob = new Blob([content], {
-        type: "octet/stream"
+  application: {
+    fullscreen: function(key, id){
+      document.body.addEventListener('keyup', function(e){
+        var elem = (document.getElementById(id) || id || document.body);
+        elem.requestFullScreen = elem.requestFullscreen || elem.msRequestFullscreen || elem.mozRequestFullScreen || elem.webkitRequestFullscreen;
+        if(e.keyCode === key){
+          elem.requestFullScreen();
+        }
       });
-
-      if (opts && opts.download && opts.download === true) {
-        PB.file.download(newBlob, opts.name);
-        return newBlob;
-      } else {
-        return newBlob;
-      }
     },
-    download: function(file, name) {
-      c = confirm('Do you want to download ' + name + "?");
-      if (c === true) {
-        // Generate data
-        url = window.URL.createObjectURL(file);
-
-        //Create hidden link
-        var a = document.createElement('a');
-        a.style = "display: none";
-        a.id = 'downloadLink';
-        a.href = url;
-        a.download = name;
-        document.body.appendChild(a);
-
-        // Download & Remove link
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(document.getElementById('downloadLink'));
-      }
-    },
-    read: function() {
-
-    }
+		pointerLock: function(key, id){
+		  document.body.addEventListener('keyup', function(e){
+        var elem = (document.getElementById(id) || id || document.body);
+        elem.requestPointerLock = elem.requestPointerLock || elem.msRequestPointerLock || elem.mozRequestPointerLock || elem.webkitRequestPointerLock;
+        if(e.keyCode === key){
+          elem.requestPointerLock();
+        }
+      });
+		}
+	},
+  keyCode: {
+		"Backspace": 8,
+		"Tab": 9,
+		"Enter": 13,
+		'Shift': 16,
+		'Ctrl': 17,
+		'Alt': 18,
+		'Pause/Break': 19,
+		'Caps_Lock': 20,
+		"Escape": 27,
+		"Space_Bar": 32,
+		'Page_Up': 33,
+		'Page_Down': 34,
+		'End': 35,
+		'Home': 36,
+		'Left_Arrow': 37,
+		'Up_Arrow': 38,
+		'Right_Arrow': 39,
+		'Down_Arrow': 40,
+		"Insert": 45,
+		"Delete": 46,
+		"0": 48,
+		"1": 49,
+		"2": 50,
+		"3": 51,
+		"4": 52,
+		"5": 53,
+		"6": 54,
+		"7": 55,
+		"8": 56,
+		"9": 57,
+		"a": 65,
+		"b": 66,
+		"c": 67,
+		"d": 68,
+		"e": 69,
+		"f": 70,
+		"g": 71,
+		"h": 72,
+		"i": 73,
+		"j": 74,
+		"k": 75,
+		"l": 76,
+		"m": 77,
+		"n": 78,
+		"o": 79,
+		"p": 80,
+		"q": 81,
+		"r": 82,
+		"s": 83,
+		"t": 84,
+		"u": 85,
+		"v": 86,
+		"w": 87,
+		"x": 88,
+		"y": 89,
+		"z": 90,
+		"Left_Window_Key": 91,
+		"Right_Window_Key": 92,
+		"Select_Key": 93,
+		"Numpad_0": 96,
+		"Numpad_1": 97,
+		"Numpad_2": 98,
+		"Numpad_3": 99,
+		"Numpad_4": 100,
+		"Numpad_5": 101,
+		"Numpad_6": 102,
+		"Numpad_7": 103,
+		"Numpad_8": 104,
+		"Numpad_9": 105,
+		"Multiply": 106,
+		"Add": 107,
+		"Subtract": 109,
+		"Decimal_Point": 110,
+		"Divide": 111,
+		"f1": 112,
+		"f2": 113,
+		"f3": 114,
+		"f4": 115,
+		"f5": 116,
+		"f6": 117,
+		"f7": 118,
+		"f8": 119,
+		"f9": 120,
+		"f10": 121,
+		"f11": 122,
+		"f12": 123,
+		"Num_Lock": 144,
+		"Scroll_Lock": 145,
+		";": 186,
+		"=": 187,
+		",": 188,
+		"-": 189,
+		".": 190,
+		"/": 191,
+		"`"	: 192,
+		"[": 219,
+		"\\": 220,
+		"]": 221,
+    "'": 222,
   },
+	file: {
+		create: function(content, opts){
+			// Generate data
+			var newBlob = new Blob([content], {type: "octet/stream"});
+			
+			if(opts && opts.download && opts.download === true){
+				PB.file.download(newBlob, opts.name);
+				return newBlob;
+			} else {
+				return newBlob;
+			}
+		},
+		download: function(file, name) {
+      if(name !== null && name !== undefined){
+        c = confirm('Do you want to download ' + name + "?");
+        if(c === true){
+          // Generate data
+          url = window.URL.createObjectURL(file);
+          
+          //Create hidden link
+          var a = document.createElement('a');
+          a.style = "display: none";
+          a.id = 'downloadLink';
+          a.href = url;
+          a.download = name;
+          document.body.appendChild(a);
+          
+          // Download & Remove link
+          a.click();
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(document.getElementById('downloadLink'));
+        }
+      } else {
+        console.error('Please give a name to your file!');
+      }
+		},
+		read: function(file){
+      var reader = new FileReader();
+      reader.addEventListener("loadend", function() {
+        return reader.result;
+      });
+      reader.readAsArrayBuffer(file);
+		}
+	},
   math: {
     area: {
       square: function(x) {
